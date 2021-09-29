534f145b25cf9593ebaaa73c106291210df5be85
Santiago Vélez Pulgarin <santiago.velez@ceiba.com.co>
2021-09-29 11:46:44 -0500

    Se ajusto la cache

--- src/main/java/com/ceiba/mercapulgas/cache/Cache.java
+++ src/main/java/com/ceiba/mercapulgas/cache/Cache.java
@@ -1,26 +1,21 @@
 package com.ceiba.mercapulgas.cache;
 
-import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-
 import javax.annotation.PostConstruct;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
-
 import com.ceiba.mercapulgas.dominio.Productos;
-import com.ceiba.mercapulgas.dominio.ProductosCache;
 
 
 @Component
 public class Cache {
 
-	private static  String key = "";
+	private  String key = "";
 	private RedisTemplate<String, Page<Productos>> redisTemplate;
-	private HashOperations hashOperations;
+	private HashOperations<String, String, Page<Productos>> hashOperations;
 	
 	public Cache(RedisTemplate<String, Page<Productos>> redisTemplate) {
 		this.redisTemplate = redisTemplate;
@@ -29,6 +24,10 @@ public class Cache {
 	
 	public void setKey(String key) {
 		this.key = key;
+	}
+	
+	public String getKey() {
+		return this.key;
 	}
 	
 	@PostConstruct
--- src/main/java/com/ceiba/mercapulgas/service/ProductosServicioImpl.java
+++ src/main/java/com/ceiba/mercapulgas/service/ProductosServicioImpl.java
@@ -1,35 +1,23 @@
 package com.ceiba.mercapulgas.service;
 
-
-
-
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
-
 import com.ceiba.mercapulgas.cache.Cache;
 import com.ceiba.mercapulgas.dao.ProductosDao;
 import com.ceiba.mercapulgas.dominio.Productos;
-import com.ceiba.mercapulgas.dominio.ProductosCache;
 
 
 @Service
 public class ProductosServicioImpl implements ProductosService{
 
-	
 	@Autowired
 	ProductosDao productosDao;
 	
 	@Autowired
 	private Cache cache;
-	
-	
 	
 	@Override
 	public Page<Productos> getProductos(int page, int size) {
@@ -40,7 +28,7 @@ public class ProductosServicioImpl implements ProductosService{
 	@Override
 	public Page<Productos> search(int page, int size, String search) {
 		cache.setKey(search);
-		Page<Productos> cacheProductos = obtenerCache(search);
+		Page<Productos> cacheProductos = obtenerCache();
 		if(cacheProductos == null) {
 			Page<Productos> paginacionProductos = productosDao.findByNombreProducto(PageRequest.of(page, size), search);
 			agregarCache(paginacionProductos);
@@ -48,21 +36,18 @@ public class ProductosServicioImpl implements ProductosService{
 		}else {
 			return cacheProductos;
 		}
-		
 	}
 	
 	private void agregarCache(Page<Productos> paginacion ) {
-		System.out.println("Agrego cache");
 			cache.save(paginacion);
-		
 	}
 	
-	private Page<Productos> obtenerCache(String search) {
-		System.out.println("Listo cache");
-		return  cache.findAll().get(search);
+	private Page<Productos> obtenerCache() {
+		Map<String, Page<Productos>> mapaProductos = cache.findAll();
+		if(mapaProductos.isEmpty()) {
+			return null;
+		}else {
+			return mapaProductos.entrySet().iterator().next().getValue();
+		}
 	}
-	
-	
-	
-
 }
